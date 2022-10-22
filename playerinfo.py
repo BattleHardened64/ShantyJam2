3ea03c98f13e6d873cad7f59725b0c3482b7af2b
BattleHardened64 <ewilliamstn001@gmail.com>
2022-10-22 01:34:06 -0500

    created firewizard sprite

--- playerinfo.py
+++ playerinfo.py
@@ -1,2 +1,13 @@
 # The player class will go here.
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    #Spawn a Player
+
+    def __init__(self) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+
+        
 
